8319318d7077d102977530a2a76d6afa0549d556
Oleh Strokan <oleg14ua71@gmail.com>
2024-09-01 11:52:22 +0200

    update governance token contract

--- src/governance/token/GovernanceToken.sol
+++ src/governance/token/GovernanceToken.sol
@@ -1,30 +1,34 @@
 // SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/extension/ERC20Votes";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-contract GovernanceToken is ERC20Votes {
-    uint256 public maxSupply = 1000000;
-
-    constructor() ERC20("VilimToken", "VLM") ERC20Permit("VilimToken") {
-        _mint(msg.sender, maxSupply);
+contract GovernanceToken is ERC20, Ownable, ERC20Permit, ERC20Votes {
+    constructor(
+        address _timelock
+    ) ERC20("VilimToken", "VLM") Ownable(_timelock) ERC20Permit("VilimToken") {
+        _mint(_timelock, 1000 * 10 ** decimals());
     }
 
-    function _afterTokenTransfer(
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _update(
         address from,
         address to,
-        uint256 amount
-    ) internal override(ERC20Votes) {
-        super._afterTokenTransfer(from, to, amount);
+        uint256 value
+    ) internal override(ERC20, ERC20Votes) {
+        super._update(from, to, value);
     }
 
-    function _mint(address, to, uint256 amount) internal override(ERC20Votes) {
-        super._mint(to, amout);
-    }
-
-    function _burn(
-        address account,
-        uint256 amount
-    ) internal override(ERC20Votes) {
-        super._burn(account, amount);
+    function nonces(
+        address owner
+    ) public view override(ERC20Permit, Nonces) returns (uint256) {
+        return super.nonces(owner);
     }
 }
