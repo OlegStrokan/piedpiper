e5036cc3e931426136594d7b984d85b2b3730017
Oleh Strokan <oleg14ua71@gmail.com>
2024-09-02 15:58:19 +0200

    update upgradeProxy contract

--- script/upgrade/UpgradeProxy.s.sol
+++ script/upgrade/UpgradeProxy.s.sol
@@ -2,31 +2,59 @@
 pragma solidity 0.8.24;
 
 import {Script} from "lib/forge-std/src/Script.sol";
-import "lib/openzeppelin-contracts/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import "lib/forge-std/src/console.sol";
 
 contract UpgradeProxy is Script {
-    address PROXY_ADDRESS = vm.envAddress("BOX_PROXY_CONTRACT_ADDRESS");
-    address NEW_IMPLEMENTATION_ADDRESS =
+    address private proxyAddress = vm.envAddress("BOX_PROXY_CONTRACT_ADDRESS");
+    address private newImplementationAddress =
         vm.envAddress("BOX_V2_CONTRACT_ADDRESS");
 
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        (bool success, ) = PROXY_ADDRESS.call(
+        console.log("Attempting to upgrade proxy at address:", proxyAddress);
+        console.log("New implementation address:", newImplementationAddress);
+
+        require(proxyAddress != address(0), "Invalid proxy address");
+        require(
+            newImplementationAddress != address(0),
+            "Invalid new implementation address"
+        );
+
+        (bool success, bytes memory returnData) = proxyAddress.call(
             abi.encodeWithSignature(
                 "upgradeTo(address)",
-                NEW_IMPLEMENTATION_ADDRESS
+                newImplementationAddress
             )
         );
+
+        if (!success) {
+            string memory revertReason = _decodeRevertReason(returnData);
+            console.log("Upgrade failed with reason:", revertReason);
+        }
         require(success, "Upgrade failed");
 
         console.log(
             "Proxy upgraded to new implementation at:",
-            NEW_IMPLEMENTATION_ADDRESS
+            newImplementationAddress
         );
 
         vm.stopBroadcast();
+    }
+
+    function _decodeRevertReason(
+        bytes memory returnData
+    ) internal pure returns (string memory) {
+        if (returnData.length < 68) {
+            return "No revert reason or invalid revert data";
+        }
+
+        bytes memory reasonData = new bytes(returnData.length - 4);
+        for (uint256 i = 4; i < returnData.length; i++) {
+            reasonData[i - 4] = returnData[i];
+        }
+
+        return abi.decode(reasonData, (string));
     }
 }
