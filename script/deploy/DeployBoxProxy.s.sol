d4af1468cfa096896a130bb87ef35b739cbfb47b
Oleh Strokan <oleg14ua71@gmail.com>
2024-09-03 11:22:35 +0200

    update deploy proxy contract

--- script/deploy/DeployBoxProxy.s.sol
+++ script/deploy/DeployBoxProxy.s.sol
@@ -2,11 +2,16 @@
 pragma solidity 0.8.24;
 
 import {Script} from "lib/forge-std/src/Script.sol";
-import "lib/openzeppelin-contracts/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import "lib/openzeppelin-contracts/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "lib/openzeppelin-contracts/contracts/proxy/transparent/ProxyAdmin.sol";
+import "lib/openzeppelin-contracts/contracts/proxy/utils/Initializable.sol";
 import "lib/forge-std/src/console.sol";
 
 contract DeployBoxProxy is Script {
-    address BOX_IMPLEMENTATION_ADDRESS = vm.envAddress("BOX_CONTRACT_ADDRESS");
+    address private implementationAddress =
+        vm.envAddress("BOX_CONTRACT_ADDRESS");
+    address private proxyAdminAddress =
+        vm.envAddress("BOX_PROXY_ADMIN_CONTRACT_ADDRESS");
 
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
@@ -14,8 +19,13 @@ contract DeployBoxProxy is Script {
 
         bytes memory data = abi.encodeWithSignature("initialize(uint256)", 42);
 
-        ERC1967Proxy proxy = new ERC1967Proxy(BOX_IMPLEMENTATION_ADDRESS, data);
-        console.log("Box proxy deployed to:", address(proxy));
+        TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(
+            implementationAddress,
+            proxyAdminAddress,
+            data
+        );
+
+        console.log("TransparentUpgradeableProxy deployed at:", address(proxy));
 
         vm.stopBroadcast();
     }
