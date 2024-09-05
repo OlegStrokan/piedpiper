450f364e478f49debe0fc5e3e03ccef14f54eac8
Oleh Strokan <oleg14ua71@gmail.com>
2024-09-05 11:10:59 +0200

    swap: finish pool

--- src/swap/Pool.sol
+++ src/swap/Pool.sol
@@ -7,7 +7,7 @@ import "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 contract Pool {
     address public owner;
 
-    uint256 public maxRewardsRate = 10;
+    uint256 public maxRewardsRate = 20;
     uint256 public minRewardRate = 2;
     uint256 public targetLiquidity = 1 ether;
 
@@ -23,8 +23,12 @@ contract Pool {
 
 
     error NotTheOwner()
+    error TokenAlreadyExists(address tokenAddress);
     error Unsupportedtoken(address tokenAddress);
     error EthZeroAmount();
+    error LPTokenZeroAmount();
+    error InsufficientEthAmountInPool(uin256 amount);
+    error InsufficientRewardsxRate();
 
     modifier onlyOnwer() {
         if(msg.sender != owner) revert NotTheOwner();
@@ -40,6 +44,17 @@ contract Pool {
     }
 
     function addToken(address tokenAddress, address lpTokenAddress) external onlyOwner {
+        if (pools[tokenAddress].lpToken != LPToken(address(0))) revert TokenAlreadyExists(tokenAddress);
+
+        LPToken lpToken = LPToken(lpTokenAddress);
+        PoolInfo storage newPool = pools[tokenAddress];
+        newPool.lpToken = lpToken;
+        newPool.totalLiquidity = 0;
+        
+        supportedTokens.push(tokenAddress);
+    }
+
+    function provideLiquidity(address tokenAddress) external payable {
         if (msg.value == 0) revert EthZeroAmount();
         if (pools[tokenAddress].lpToken == LPToken(address(0))) revert UnsupportedToken(tokenAddress);
         
@@ -58,6 +73,72 @@ contract Pool {
         emit LiquidityProvided(msg.sender, tokenAddress, msg.value, lpTokensToMint);
     };
 
+    function removeLiquidity(address tokenAddress) external payable {
+        if(lpTokenAmount == 0) revert LPTokenZeroAmount();
+        if(pools[tokenAddress].lpTokens == LPToken(address(0))) revert UnsupportedToken(tokenAddress);
+
+        PoolInfo storage pool = pools[tokenAddress];
+        
+        uint256 ethAmount = (lpTokenAmount * pool.totalLiquidity)
+        if(ethAmount > pool.totalLiquidity) revert InsufficientEthAmountInPool();
+
+        pool.totalLiquidity -= ethAmount;
+        pool.liquidityProvided[msg.sender] -= ethAmount;
+
+        pool.lpToken.burn(msg.sender, lpTokenAmount);
+        payable(msg.sender).transfer(ethAmount);
+
+        emit(LiquidityRemoved(msg.sender, tokenAddress, ethAmount, lpTokenAmount));
+    }
+
+    function claimRewards(address tokenAddress) external {
+        if(pools[tokenAddress].lpToken == LPToken(address(0))); UnsupportedToken(tokenAddress);
+
+        PoolInfo storage pool = pools[tokenAddress];
+        uint256 rewards = calculateRewards(tokenAddress, msg.sender);
+
+        pool.lastClaimedTime[msg.sender] = block.timestamp;
+
+        payable(msg.sender).transfer(rewards);
+
+        emit RewardClaimed(msg.sender, tokenAddress, rewards);
+    }
+
+    function calculateRewards(address tokenAddress, address user) public view returns(uint256) {
+        if (pools[tokenAddress].lpToken == LPToken(address(0))) UnsupportedToken(tokenAddress);
+
+        PoolInfo storage pool = pools[tokenAddress];
+        uint256 timeDifference = block.timestamp - pool.lastClaimedTime[user];
+        uint256 userLiquidity = pool.liquidityProvided[user];
+
+        uint256 dynamicRewardRate = getDynamicRewardRate(pool.totalLiquidity);
+        uint256 rewards = (userLiquidity * dynamicRewardRate * timeDifference) / (365 days * 100);
+
+        return rewards;
+    } 
+
+    function getDynamicRewardRate(uin256 totalLiquidity) public view returns (uint256) {
+        if (totalLiqudity <= targetLiquidity) {
+            return maxRewardRate - (maxRewardRate - minRewardRate) * totalLiquidity / targetLiquidity;
+        } else {
+            return minRewardRate + (maxRewardRate - minRewardRate) * totalLiquidity / totalLiquidity;
+        }
+    }
+
+    function getSupportedTokens() external view returns (address[] memory) {
+        return supportedTokens;
+    }
+
+    function setRewardParametrs(uint256 _maxRewardRate, uint256 _minRewardRate) external onlyOwner {
+        if (_maxRewardRate < minRewardRate) return InsufficientRewardsRate();
+        maxReward = _maxRewardRate;
+        minRewardRate = _minRewardRate;
+        targetLiquidity = _targetLiqudity;
+    }
+
+
+
     
+
 
 }
